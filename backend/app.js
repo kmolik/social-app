c83f36694b766550fce3ab46267738b250dc7f4d
kmolik <48009091+kmolik@users.noreply.github.com>
2022-08-04 09:51:57 +0200

    authorization for backend and frontend added

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, x-access-token');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
     next();
 });
